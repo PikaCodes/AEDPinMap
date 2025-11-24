ccb447bbe810dede271804a35f720c0b2cb12e40
PikaCodes <91448056+PikaCodes@users.noreply.github.com>
2025-11-24 14:22:49 -0800

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,18 @@
+const AED_LOCATIONS = [
+  {
+    name: "Friday Harbor High School",
+    coords: [48.53585515719564, -123.02076673291823],
+    description: "The AED is located at the main entrances; you'll need to be allowed in to enter."
+  },
+  {
+    name: "AED Pin Test 2",
+    coords: [48.53463153713263, -123.01754179399637],
+    description: "pray the lord that this works"
+  },
+  {
+    name: "AED Pin Test 3",
+    coords: [48.53569310294545, -123.01794009724279],
+    description: "idk"
+  },
+  {
+];
